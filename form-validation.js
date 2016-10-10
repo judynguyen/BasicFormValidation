2320c621d7b928af89232b1bb1eea1b17afab77c
Jude <jnguyen41@mail.csuchico.edu>
2016-10-10 16:58:30 -0700

    password validation if/else statement added

--- form-validation.js
+++ form-validation.js
@@ -4,7 +4,7 @@ function validate(){
   var passEntered = document.getElementById("pass").value;
   var userLength = userEntered.length;
   console.log("Username length is " +   userLength);
-  
+
   if(userLength>=6)
   {
     document.getElementById("usernameError").innerHTML="Good username.";
@@ -22,10 +22,20 @@ function validate(){
     document.getElementById("usernameGroup").classList.add("has-error");
   }
 
-  //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  if(passEntered == "password")
+  {
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else {
+    document.getElementById("passwordError").innerHTML="Good password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //turns elements green
+    document.getElementById("passwordGroup").classList.add("has-success");
+
+  }
 }
