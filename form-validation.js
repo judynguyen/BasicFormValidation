2a864b076ac4fe06b02b0adc26f96623cb84a531
Jude <jnguyen41@mail.csuchico.edu>
2016-10-10 16:49:06 -0700

    username length input

--- form-validation.js
+++ form-validation.js
@@ -2,13 +2,25 @@ function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
-
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  var userLength = userEntered.length;
+  console.log("Username length is " +   userLength);
+  
+  if(userLength>=6)
+  {
+    document.getElementById("usernameError").innerHTML="Good username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //turns elements green
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else {
+    //Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
 
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
